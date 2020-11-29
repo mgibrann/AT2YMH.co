3ec007bc99502cf273d97f4ead0dbb4d315ab4db
Muhamad Gibran <49028961+M-Gibran@users.noreply.github.com>
2020-11-29 19:11:05 +0700

    Application Finishedm, API change to client

--- app/services/Fire/index.js
+++ app/services/Fire/index.js
@@ -1,13 +1,23 @@
 import firebase from 'firebase';
 
+// firebase.initializeApp({
+//   apiKey: 'AIzaSyBY66D5-nwiF3nDSw80yIHuX2HZLNSux24',
+//   authDomain: 'at2ymh.firebaseapp.com',
+//   databaseURL: 'https://at2ymh.firebaseio.com',
+//   projectId: 'at2ymh',
+//   storageBucket: 'at2ymh.appspot.com',
+//   messagingSenderId: '898723431049',
+//   appId: '1:898723431049:web:99c81a0edf259769babc58',
+// });
+
 firebase.initializeApp({
-  apiKey: 'AIzaSyBY66D5-nwiF3nDSw80yIHuX2HZLNSux24',
-  authDomain: 'at2ymh.firebaseapp.com',
-  databaseURL: 'https://at2ymh.firebaseio.com',
-  projectId: 'at2ymh',
-  storageBucket: 'at2ymh.appspot.com',
-  messagingSenderId: '898723431049',
-  appId: '1:898723431049:web:99c81a0edf259769babc58',
+  apiKey: 'AIzaSyBwMe_T7YATPWP-CADSOnCxC1DvprEIGi0',
+  authDomain: 'at2ymh-ee2f9.firebaseapp.com',
+  databaseURL: 'https://at2ymh-ee2f9.firebaseio.com',
+  projectId: 'at2ymh-ee2f9',
+  storageBucket: 'at2ymh-ee2f9.appspot.com',
+  messagingSenderId: '319021226532',
+  appId: '1:319021226532:web:ebb030ac412011611816b1',
 });
 
 const Firebase = firebase;
